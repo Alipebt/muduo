a91d9e55ad1ad9785506205165f779283e6430c4
JiangZhu-N <1160756683@qq.com>
2022-11-28 18:26:59 +0800

    c++

---
+++ C++.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Animals
+{
+public:
+    void which()
+    {
+        cout << "Dog" << endl;
+    }
+};
+
+class Cat : Animals
+{
+public:
+    void which()
+    {
+        cout << "Cat" << endl;
+    }
+};
+
+int main()
+{
+    Animals a;
+    Cat c;
+
+    a.which();
+    c.which();
+}
