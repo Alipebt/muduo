3d4cee7d0e99d5b46afe8b4f05f2810ca2fe7a9a
Alipebt <1160756683@qq.com>
2022-12-31 20:56:09 +0800

    mutex

--- MutexLock.h
+++ MutexLock.h
@@ -60,3 +60,35 @@ private:
 };
 
 #define MutexLockGuard(x) static_assert(false, "missing mutex guard var name")
+
+class Condition : boost::noncopyable
+{
+public:
+    explicit Condition(MutexLock &mutex) : mutex_(mutex)
+    {
+        pthread_cond_init(&pcond_, NULL);
+    }
+    ~Condition()
+    {
+        pthread_cond_destroy(&pcond_);
+    }
+
+    void wait()
+    {
+        pthread_cond_wait(&pcond_, mutex_.getPthreadMutex());
+    }
+
+    void notify()
+    {
+        pthread_cond_signal(&pcond_);
+    }
+
+    void notifyAll()
+    {
+        pthread_cond_broadcast(&pcond_);
+    }
+
+private:
+    MutexLock &mutex_;
+    pthread_cond_t pcond_;
+};
